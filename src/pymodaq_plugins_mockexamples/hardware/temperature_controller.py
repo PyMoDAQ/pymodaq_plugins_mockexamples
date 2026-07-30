aee1919abe79c56c8af0370ba2b6c1f85d4d6bad
Sébastien Weber <seba.weber@gmail.com>
2026-07-30 21:12:03 +0200

    adding a temp controller simulating an eurotherm

--- src/pymodaq_plugins_mockexamples/hardware/temperature_controller.py
+++ src/pymodaq_plugins_mockexamples/hardware/temperature_controller.py
@@ -9,43 +9,76 @@ from pymodaq import Q_
 from simple_pid import PID
 
 
-class HeaterController(QObject):
+class TempController(QObject):
     _current_temperature = 20.
     _ambiant_temperature = 19.
     _noise = 0.1
 
-    def __init__(self, ):
+    def __init__(self, kp=0.1, ki=0., kd=0.):
         super().__init__()
         self._current_power = 0.
-        self._ellapsed_time = Q_(0., 's')
-        self._target_temperature = self._ambiant_temperature
-        self._tau = Q_(1, 's')
+        self._ellapsed_time = 0.0
+        self._last_output = 0.
 
         self.pid_timer = QTimer()
+        self.pid_timer.timeout.connect(self.update_temperature)
+        self.pid_timer.setInterval(10)
 
+        self.pid = PID(kp, ki, kd, setpoint=self._ambiant_temperature)
+
+        self.pid_timer.start()
+
+    def pause(self, do_pause=True):
+        self.pid.set_auto_mode(do_pause, last_output=self._last_output)
+
+    @property
+    def kp(self):
+        return self.pid.Kp
+
+    @kp.setter
+    def kp(self, value: float):
+        self.pid.Kp = value
+
+    @property
+    def ki(self):
+        return self.pid.Ki
+
+    @ki.setter
+    def ki(self, value: float):
+        self.pid.Ki = value
+
+    @property
+    def kd(self):
+        return self.pid.Kd
+
+    @kd.setter
+    def kd(self, value: float):
+        self.pid.Kd = value
 
     def update_temperature(self):
-        dt = Q_(perf_counter(), 's') - self._ellapsed_time
-        self._ellapsed_time += dt
+        dt = perf_counter() - self._ellapsed_time
+        self._ellapsed_time  = perf_counter()
 
-        self._current_temperature += 1 * self._current_power * dt.m_as('s') + self._noise * (random() - 0.5)
+        self._current_temperature += 1 * self._current_power * dt + self._noise * (random() - 0.5)
         # some heat dissipation
-        self._current_temperature -= 0.2 * dt.m_as('s')
+        self._current_temperature -= 0.2 * dt
+        #not below ambiant
         self._current_temperature = np.clip(self._current_temperature, self.ambiant_temp, None)
+
+        self._current_power = self.pid(self._current_temperature)
+
 
     @property
     def temperature(self):
         return self._current_temperature
 
     @temperature.setter
-    def temperature(self, value):
-        self._target_temperature = value
+    def temperature(self, value: float):
+        self.pid.setpoint = value
 
-    def check_position(self):
-        return self._current_power
-
-    def move_abs(self, value):
-        self._current_power = value
+    @property
+    def target_temperature(self):
+        return self.pid.setpoint
 
     @property
     def ambiant_temp(self):
@@ -62,12 +95,3 @@ class HeaterController(QObject):
     @noise.setter
     def noise(self, noise):
         self._noise = noise
-
-    def move_rel(self, value):
-        self._current_power += value
-
-    def grab(self):
-        return self._current_temperature
-
-
-
