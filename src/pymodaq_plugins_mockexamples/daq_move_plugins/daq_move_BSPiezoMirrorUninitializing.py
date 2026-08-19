78a767fa25f6caee0a968a0583738713a2f337c4
Sébastien Weber <seba.weber@gmail.com>
2026-08-19 15:00:07 +0200

    removing imports

--- src/pymodaq_plugins_mockexamples/daq_move_plugins/daq_move_BSPiezoMirrorUninitializing.py
+++ src/pymodaq_plugins_mockexamples/daq_move_plugins/daq_move_BSPiezoMirrorUninitializing.py
@@ -1,13 +1,7 @@
-from pymodaq_plugins_mockexamples.daq_move_plugins.daq_move_BSPiezoMirror import DAQ_Move_BSPiezoMirror
+from pymodaq.control_modules.move_utility_classes import main  # base class
 
-from pymodaq.control_modules.move_utility_classes import DAQ_Move_base, main  # base class
-from pymodaq.control_modules.move_utility_classes import comon_parameters_fun  # common set of parameters for all actuators
-
-from pymodaq.utils.daq_utils import ThreadCommand, getLineInfo  # object used to send info back to the main thread
-from easydict import EasyDict as edict  # type of dict
-from pymodaq_plugins_mockexamples.hardware.beam_steering import BeamSteering, BeamSteeringActuators
-
-from pymodaq_plugins_mockexamples import config
+from pymodaq_plugins_mockexamples.daq_move_plugins.daq_move_BSPiezoMirror import (
+    DAQ_Move_BSPiezoMirror, BeamSteering)
 
 
 class DAQ_Move_BSPiezoMirrorUninitializing(DAQ_Move_BSPiezoMirror):
